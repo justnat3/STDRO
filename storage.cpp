d5a9005e15f439a70415a541870d5da8b53d5b3f
Nathan Reed <vemhira@gmail.com>
2023-05-15 23:29:01 -0500

    Update storage.cpp

--- storage.cpp
+++ storage.cpp
@@ -1,14 +1,14 @@
 #include <Arduino.h>
 #include "storage.hpp"
 
-struct owsd_page* alloc_owsd_page(char byte) {
-    struct owsd_page *new_page = 
+struct owsd_page* alloc_owsd_page(uint8_t chunk[]) {
+    struct owsd_page *new_page =
         (struct owsd_page*) malloc(sizeof(struct owsd_page*));
 
     if (new_page == NULL) return NULL;
 
-    new_page->data = byte;
+    new_page->chunk = (uint8_t*)malloc(sizeof(uint8_t*)*CHUNK_SIZE);
     new_page->next = NULL;
     new_page->prev = NULL;
-    return new_page;
+    return NULL;
 }
